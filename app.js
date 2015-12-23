6bd903a0716a3d51d380261bf9a960171dd329b9
Ayush Pandey <Ayush Pandey>
2015-12-23 21:30:06 +0530

    Changed index.html

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
   app.controller('StoreController', ['$http', function($http){
     var store = this;
     store.products = [];
-    $http.get('./store-products.json').success(function(data){
+    $http.get('http://api.clozerr.com/v2/vendor/search/near/?latitude=10&longitude=10&offset=0').success(function(data){
         store.products = data;
     });
   }]);
