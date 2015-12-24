d8d75531dbe75176da154b7020a2455e9d05ff0d
Ayush Pandey <Ayush Pandey>
2015-12-25 02:25:36 +0530

    Added Header

--- fb-login.js
+++ fb-login.js
@@ -3,8 +3,8 @@
  */
 // This is called with the results from from FB.getLoginStatus().
 function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
+    //console.log('statusChangeCallback');
+    //console.log(response);
     // The response object is returned with a status field that lets the
     // app know the current login status of the person.
     // Full docs on the response object can be found in the documentation
@@ -16,7 +16,7 @@ function statusChangeCallback(response) {
     } else if (response.status === 'not_authorized') {
         // The person is logged into Facebook, but not your app.
         document.getElementById('status').innerHTML = 'Please log ' +
-            'into this app.';
+            'into Clozerr.';
     } else {
         // The person is not logged into Facebook, so we're not sure if
         // they are logged into this app or not.
