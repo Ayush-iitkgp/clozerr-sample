0c2190ca5ccd64cb87c03a2875cf7961d02d81e9
Ayush Pandey <Ayush Pandey>
2015-12-25 03:32:44 +0530

    Created HomePage

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
     var store = this;
     store.products = [];
     $http.get('http://api.clozerr.com/v2/vendor/search/near/?latitude=10&longitude=10&offset=0').success(function(data){
-        store.products = data;
+      store.products = data;
     });
   }]);
 
